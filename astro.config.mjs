73c3a33da75f23095d8145d24d33b83838f834d0
Carlos Gallaga <carlos.gallagav@gmail.com>
2025-02-23 00:52:18 -0600

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import astroIcon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(), astroIcon({
+      include: {
+        mdi: ["*"],
+        'ri': ['*'],
+        'simple-icons': ['*']
+      }
+    })
+  ]
 });
